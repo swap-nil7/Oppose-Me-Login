7ffec16b92fe6b844077d3b449cdb58c29dcf2b2
Swapnil Negi <swapnil.negi09@gmail.com>
2017-07-11 19:11:28 +0530

    started on database

--- social/models.py
+++ social/models.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Stats(models.Model):
+	username = models.CharField(max_length=100)
+	score = models.IntegerField(default=0)
