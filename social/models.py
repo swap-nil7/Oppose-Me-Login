483c9db63bd761897ee22be40b908369820083fa
Swapnil Negi <swapnil.negi09@gmail.com>
2017-07-12 16:45:17 +0530

    added model for leaderboard

--- social/models.py
+++ social/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Stats(models.Model):
 	username = models.CharField(max_length=100)
 	score = models.IntegerField(default=0)
+
+class Leaderboard(models.Model):
+	username = models.CharField(max_length=100)
+	score = models.IntegerField(default=0)
