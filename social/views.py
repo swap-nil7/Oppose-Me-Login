8a92fae20b0737c6b6c9c203d3b6de40d3f6e3f2
Swapnil Negi <swapnil.negi09@gmail.com>
2017-07-13 23:56:32 +0530

    added leaderboard

--- social/views.py
+++ social/views.py
@@ -10,8 +10,10 @@ def index(request):
 def home(request):
 	user = request.user.username
 	scores = Stats.objects.filter(username=user).order_by('-score')[:5]
+	highs = Stats.objects.all().order_by('-score')[:10]
 	context = {
 	'scores': scores,
+	'highs': highs,
 	}
 	return render(request, 'social/home.html', context)
 
