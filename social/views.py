c05475ca08afb260c2b597c5fb7078aaad63774e
Swapnil Negi <swapnil.negi09@gmail.com>
2017-07-14 23:26:06 +0530

    added recent scores

--- social/views.py
+++ social/views.py
@@ -12,9 +12,11 @@ def home(request):
 	user = request.user.username
 	scores = Stats.objects.filter(username=user).order_by('-score')[:5]
 	highs = Stats.objects.all().order_by('-score')[:10]
+	recent = Stats.objects.filter(username=user).order_by('-pk')[:5]
 	context = {
 	'scores': scores,
 	'highs': highs,
+	'recent': recent,
 	}
 	return render(request, 'social/home.html', context)
 
